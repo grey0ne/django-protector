c1c8df0fdb0b92973d066f51a09919346edc1261
Vasili Ivanov <vasili.ivanov@corp.mail.ru>
2019-08-01 17:28:08 +0300

    admin panel now takes into account history as well

--- protector/reserved_reasons.py
+++ protector/reserved_reasons.py
@@ -4,5 +4,11 @@ TEST_REASON = 'TEST REASON'
 GENERIC_GROUP_UPDATE_REASON = 'Groups were created due to foreign key links update'
 
 
+def ADMIN_PANEL_DELETE_REASON(user):
+    if not user or not user.username:
+        user = 'unknown user'
+    return 'Relation was deleted through admin panel by {}'.format(user.username)
+
+
 def MEMBER_FK_UPDATE_REASON(field):
     return 'User was added to group due to {} foreign key update'.format(field)
