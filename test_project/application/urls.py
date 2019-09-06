2ac2e0f0ad772239173f99fec6c9af8841f23ac2
Vasili Ivanov <vasili.ivanov@corp.mail.ru>
2019-09-06 13:51:35 +0300

    removed forgotten backend

--- test_project/application/urls.py
+++ test_project/application/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = (
+)
