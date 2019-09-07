cb87812ef3d17a49082c53a4840b05c3d3e4e191
Sergey Lihobabin <greyone@greyone.ru>
2019-09-07 20:33:24 +0300

    fixed python2 compatibility

--- test_project/application/urls.py
+++ test_project/application/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
 
-urlpatterns = (
-    path('admin/', admin.site.urls),    
-)
+try:
+    from django.urls import path
+    urlpatterns = (
+        path('admin/', admin.site.urls),    
+    )
+except ImportError:
+    from django.conf.urls import url, include
+    urlpatterns = (
+        url(r'^admin/', include(admin.site.urls)),    
+    )
