9a5efadcdc4c087f82b970596f4fd0c5f3146f6d
d.epifanov <d.epifanov@vk.team>
2022-10-24 12:18:06 +0300

    Delete protector entry group while removing entry.
    Let's remove dependent entries in GenericUserToGroup model while removing entry, restricted by Protector. Goal is avoiding obsolete entries after removing.

--- protector/models.py
+++ protector/models.py
@@ -479,6 +479,7 @@ class AbstractGenericGroup(GenericPermsMixin):
 
     class Meta:
         abstract = True
+        delete_protector_group = True
 
     def __init__(self, *args, **kwargs):
         super(AbstractGenericGroup, self).__init__(*args, **kwargs)
@@ -487,6 +488,12 @@ class AbstractGenericGroup(GenericPermsMixin):
     def save(self, *args, **kwargs):
         super(AbstractGenericGroup, self).save(*args, **kwargs)
         self._update_member_foreign_key()
+
+    def delete(self, *args, **kwargs):
+        result = super().delete(*args, **kwargs)
+        if self.Meta.delete_protector_group:
+            GenericUserToGroup.objects.filter(group_id=self.pk, group_content_type=type(self).__name__.lower())
+        return result
 
     def _update_member_foreign_key(self):
         for field, roles in self.MEMBER_FOREIGN_KEY_FIELDS:
